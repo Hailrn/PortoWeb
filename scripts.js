55e67e23ebc2e1943223cbf47d292597ad766709
Hailrn <xwildan07@gmail.com>
2025-07-03 11:07:37 +0700

    progress 2

---
+++ scripts.js
@@ -0,0 +1,36 @@
+const sections = document.querySelectorAll('section');
+const navLinks = document.querySelectorAll('header nav a');
+
+window.addEventListener('scroll', () => {
+    let current = '';
+    sections.forEach(section => {
+        const sectionTop = section.offsetTop;
+        if (pageYOffset >= sectionTop - 150) {
+        current = section.getAttribute('id');
+        }
+    });
+
+    navLinks.forEach(link => {
+        link.classList.remove('text-blue-600', 'font-bold');
+        if (link.getAttribute('href').includes(current)) {
+        link.classList.add('text-blue-600', 'font-bold');
+        }
+    });
+
+    
+});
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('is-visible');
+      } else {
+        entry.target.classList.remove('is-visible');
+      }
+    });
+  });
+
+const sectionsToAnimate = document.querySelectorAll('section');
+sectionsToAnimate.forEach(section => {
+  section.classList.add('fade-in-section');
+  observer.observe(section);
+});
